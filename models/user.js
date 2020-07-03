7bb7d086c64121fe2c67a4dadf59c4142fafd7ed
Joewan Eiland <Joewan.Eiland@gmail.com>
2020-07-02 21:35:43 -0700

    Added user.js with attributes

---
+++ models/user.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, DataTypes){
+
+    const User = sequelize.define("user",{
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        balance: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+ });
+return User;
+};
