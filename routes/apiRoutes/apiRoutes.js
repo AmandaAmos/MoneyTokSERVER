934be4fab0c1098b2ed2352f496597d397862f72
Joewan Eiland <Joewan.Eiland@gmail.com>
2020-07-06 22:27:14 -0700

    Added user routes that create user, get all users or get specific user

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,17 +1,56 @@
 ///Dependencies
 //======================================================
-var db = require("../models");
+const db = require("../models");
 
 
 //Routes
 //=========================================================
 module.exports = function(app) {
 
-    
-    
-    
-    
-    
+//Get all users
+app.get("/api/user", function(req, res) {
+    db.user.findAll({}).then(function(dbuser) {
+      res.json(dbuser);
+    });
+  });
+
+// //Gets a specific user
+app.get("/api/user/:id", function(req, res) {
+    // Find one user with the id in req.params.id and return them with res.json
+    db.user.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbuser) {
+      res.json(dbuser);
+    });
+  });
+
+  //create new user
+  app.post("/api/user", function (req, res) {
+
+     db.user.create({
+         firstname : req.body.firstname,
+         lastname: req.body.lastname,
+         email: req.body.email,
+         username: req.body.username,
+         password: req.body.password,
+         balance: req.body.balance
+
+ 
+     }).then(function (user) {
+        // send back the user id to client
+        res.json(user.id);
+         console.log("Inserted a new user into our user table!");
+     }).catch(function(err){
+       //if error
+         console.log(err);
+         //sends an error to the client
+         res.send(false);
+     })
+ })
+
+
     
     
     
