dd9a178005c07592250055fa918d2d46ce12438a
Steffi Wellington <steffisbootcampdrive@gmail.com>
2020-07-06 21:07:08 -0700

    transaction model and associations

--- models/user.js
+++ models/user.js
@@ -36,6 +36,11 @@ module.exports = function(sequelize, DataTypes){
          onDelete: "cascade"
      });
  };
- 
+ User.associate = function(models) {
+    User.hasMany(models.Transaction, {
+        onDelete: "cascade"
+    });
+};
+
 return User;
 };
