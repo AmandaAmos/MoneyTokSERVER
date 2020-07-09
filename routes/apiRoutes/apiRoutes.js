1c9bcc8bfe714d8f282974aa96ea91a9b214050f
Joewan Eiland <Joewan.Eiland@gmail.com>
2020-07-08 21:36:16 -0700

    Added routes for getting all and specific budgets/transactions

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -14,6 +14,44 @@ app.get("/api/user", function(req, res) {
     });
   });
 
+  //get all transactions (maybe if we wanted to see what most users spend on??)
+  app.get("/api/transaction", function(req, res) {
+    db.Transaction.findAll({}).then(function(dbTransaction) {
+      res.json(dbTransaction);
+    });
+  });
+
+  //get all budgets (maybe if we were trying to see a trend)
+  app.get("/api/budget", function(req, res) {
+    db.Budget.findAll({}).then(function(dbBudget) {
+      res.json(dbBudget);
+    });
+  });
+
+// //Gets a specific transaction
+app.get("/api/transaction/:id", function(req, res) {
+    // Find one Transaction with the transid in req.params.id and return them with res.json
+    db.Transaction.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbTransaction) {
+      res.json(dbTransaction);
+    });
+  });
+
+// //Gets a specific user budget
+app.get("/api/budget/:id", function(req, res) {
+    // Find a specific user budget with the id in req.params.id and return them with res.json
+    db.Budget.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbBudget) {
+      res.json(dbBudget);
+    });
+  });
+
 // //Gets a specific user
 app.get("/api/user/:id", function(req, res) {
     // Find one user with the id in req.params.id and return them with res.json
@@ -81,7 +119,7 @@ app.post("/api/transaction", function (req, res) {
 
 
     }).then(function (Transaction) {
-       // send back the budget id to client
+       // send back the transaction id to client
        res.json(Transaction.id);
         console.log("Reported a new user transaction into our transaction table!");
     }).catch(function(err){
